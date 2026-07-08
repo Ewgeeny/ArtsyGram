aed3bb6d5a3d921750b8ee6fab7a15ad0e5562b2
Ewgeeny <ewgenia.sterleadova@study.hs-duesseldorf.de>
2026-07-09 01:03:38 +0900

    Implement initial database schema for ArtsyGram

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Tag, Post, Collection
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Collection)
--- core/models.py
+++ core/models.py
@@ -1,1 +1,39 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    image_path = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag)
+    upload_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Collection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    posts = models.ManyToManyField(Post)
+    creation_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
