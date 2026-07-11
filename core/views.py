84fc4a13e19fa85127cda6df63e5be49c316cf4d
Ewgeeny <ewgenia.sterleadova@study.hs-duesseldorf.de>
2026-07-11 15:50:03 +0900

    Implement basic view functions and URL routing

--- core/views.py
+++ core/views.py
@@ -1,1 +1,36 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+from .models import Post
+
+
+def index(request):
+    return HttpResponse("<h1>Welcome to ArtsyGram!</h1>")
+
+
+def post_list(request):
+    posts = Post.objects.all()
+
+    result = "<h1>All Posts</h1>"
+
+    for post in posts:
+        result += f"<p>{post.title}</p>"
+
+    return HttpResponse(result)
+
+
+def photography_posts(request):
+    posts = Post.objects.filter(
+        category__name="Photography"
+    ).order_by("-upload_date")
+
+    result = "<h1>Photography Posts</h1>"
+
+    for post in posts:
+        result += f"<p>{post.title}</p>"
+
+    return HttpResponse(result)
+
+
+def go_home(request):
+    return redirect("view-index")
